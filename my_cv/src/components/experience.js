796d4be0dbba3f72ef7023d4eff320d4060fb9d2
Anna Rudrnko <rudenkoanna1992@gmail.com>
2023-05-04 10:12:59 +0300

    created experience component

---
+++ my_cv/src/components/experience.js
@@ -0,0 +1,35 @@
+function Experience() {
+  return (
+    <div className="experience">
+      <h2>Work Experience</h2>
+      <hr />
+      <p>
+        <strong> Name of employer: </strong> UvoCorp
+      </p>
+      <p>
+        <strong> Dates of employment: </strong> 2013 - 2019
+      </p>
+      <p>
+        <strong> Job title: </strong> Support Team Member
+      </p>
+      <p>
+        <strong> Project/Role Description: </strong> Providing an assistance for
+        English speaking customers from all over the Globe.
+      </p>
+      <br />
+      <p>
+        <strong> Name of employer: </strong> UvoCorp
+      </p>
+      <p>
+        <strong> Dates of employment: </strong> 2018 - 2019
+      </p>
+      <p>
+        <strong> Job title: </strong> English Academic Writer
+      </p>
+      <p>
+        <strong> Project/Role Description: </strong> Writing the warious
+        content, projects, researches, making some copyright texts.
+      </p>
+    </div>
+  );
+}
